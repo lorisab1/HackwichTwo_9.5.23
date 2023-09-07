5806dc90da950ddcfd6ea1fbdb0ce933128e8742
Lorisa Bulosan <lorisab@hawaii.edu>
2023-09-06 23:13:29 -1000

    Completed HW2 Part 8: On your own

--- HackwichTwo_9.5.23/ViewController.swift
+++ HackwichTwo_9.5.23/ViewController.swift
@@ -8,20 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //Part 8: This is where I want you to declare 3 string variables
+    var one = "Welcome"
+    var two = " User"
+    var three = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        three = one + two
     }
 
-
+    
     @IBAction func pressMeButtonPressed(_ sender: Any)
     {
         //code block
-        print("Hello World")
+        print("Greetings")
     }
     
     
+    @IBAction func pressMeButton2Pressed(_ sender: Any)
+    {
+        print(three)
+    }
     
 }
 
